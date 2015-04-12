7473c82cb29e68f8743f18cf63fe49ce95aa5396
Juan Nunez <njuan1993@yahoo.com>
2015-04-12 01:24:16 -0400

    Disabled control Z and C

--- shell/main.c
+++ shell/main.c
@@ -3,21 +3,24 @@
 
 void shellInit(){
 	n_commands=0;
-	aliasNode=NULL; //check this out.
+	aliasNode=NULL;  
+ 	 signal(SIGQUIT, SIG_IGN); 
+  	 signal(SIGTSTP, SIG_IGN); 
+  	 signal(SIGINT, SIG_IGN);
 }
+
 
 int main(){
 
-	//CommandTable[MAXCMDS]= (COMMAND*) malloc((MAXCMDS)*sizeof(COMMAND));
-	//Command = (COMMAND*)malloc(sizeof(COMMAND));
-	//Command->args = (char*)malloc(sizeof(char)*MAXARGS);
-	//metaChars[MAXMETACHARS]=(char*) malloc((MAXMETACHARS)*sizeof(char*));  //might fail in the future 
 shellInit();
 while(1){
   printPrompt();
-//  fgets(input,sizeof(input),stdin);
-// yy_scan_string(input);
+  fgets(input,sizeof(input),stdin);
+ yy_scan_string(input);
   yyparse();
   }
+
+
+
 
 } 
